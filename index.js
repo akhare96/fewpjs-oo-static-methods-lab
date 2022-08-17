4c2e822a70090b2939c108c641a342de515c8782
Ambar Khare <ambar.khare24@gmail.com>
2022-08-16 17:00:20 -0700

    commit tree 6a95c7f6101a0164804a863d57554592a82263e7

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+
+
+  static titleize(sentence) {
+    const skipCapitalize = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let array1 = sentence.split(" ")
+    return array1.map(function(word) {
+      if (skipCapitalize.includes(word)) {
+        return word
+      } else {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    }}).join(" ")
+  }
+
 }
